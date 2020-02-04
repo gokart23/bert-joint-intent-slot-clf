d41a04157ef9809ea7c0c42ecbc5069bbbb2faa8
Karthik Duddu <karmeher95+github@gmail.com>
2020-02-03 19:58:40 -0500

    WIP: Added functional CLI interface for using trained model

---
+++ src/cli.py
@@ -0,0 +1,51 @@
+#!/bin/env python3
+import config as cfg
+import src.utils as utils
+import src.data.make_dset as dset
+from src.models.bert_fc import BertFC
+
+import torch
+import pickle as pkl
+import os, argparse
+from transformers import BertTokenizer
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--model', type=str, required=True)
+parser.add_argument('--test-dset', type=str, 
+                    default='data/interim/atis.test.pkl')
+
+args = parser.parse_args()
+if not os.path.isfile(args.model):
+    print ("Invalid model filepath %s - please provide valid path"
+           % (args.model))
+
+with open(args.test_dset, 'rb') as f:
+    pkl_dict = pkl.load(f)
+    intent_dict, slot_dict = pkl_dict['intent_dict'], pkl_dict['slot_dict']
+    rev_intent, rev_slot = {v:k for k,v in intent_dict.items()}, {v:k for k,v in slot_dict.items()}
+
+num_intent_classes, num_slot_classes = len(intent_dict.keys()), len(slot_dict.keys())
+model = BertFC(num_intent_classes=num_intent_classes,
+               num_slot_classes=num_slot_classes)
+print ("Attempting to load trained model with %d intent classes"
+       " and %d slot classes" % (num_intent_classes, num_slot_classes))
+model.load_state_dict(torch.load(args.model))
+model = model.to(cfg.device)
+model.eval()
+
+print ("Model loaded successfully, max token length is %d" % (cfg.MAX_TOK_LEN))
+
+tokenizer = BertTokenizer.from_pretrained(cfg.MODEL_TYPE,
+                                          do_basic_tokenize=False,
+                                          do_lower_case=True)
+# REPL loop
+while True:
+    print ("> ")
+    orig_sentence = input()
+    sentence = dset.cls_token + ' ' + orig_sentence + ' ' + dset.sep_token
+    inp = dset.conv_to_idxes(tokenizer, [sentence], cfg.MAX_TOK_LEN)
+    tokens, rel_slot_mask, attn_mask = (torch.LongTensor(t).to(cfg.device) for t in inp)
+    logits = model(tokens, rel_slot_mask, attn_mask)
+    intent_preds, slot_preds = (torch.argmax(t, dim=1).tolist() for t in logits)
+    print ("Intent: ", rev_intent[intent_preds[0]])
+    print ("Slots: ", ' '.join(["%s(%s)" % (w, rev_slot[x].upper()) for w, x in zip(orig_sentence.split(), slot_preds)]))
